dfc57ff40a97591c7ff98dc7cefdfcaae66e98dc
Mr.L <18801261225@163.com>
2019-07-09 21:22:43 +0800

    1

--- app/Index/admin.py
+++ app/Index/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Users,Articles,Category,Leave
+from .models import Users, Articles, Category, Leave
 
 admin.site.register(Articles)
 admin.site.register(Users)
