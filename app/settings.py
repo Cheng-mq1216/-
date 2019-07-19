0ce3027808150cef6782b65599f13cad52498dcb
Mahaoqu <mahaoqu@qq.com>
2019-07-19 11:18:02 +0800

    修正markdown渲染问题。添加代码高亮。
    升级mdeditor，修复h3元素显示问题。
    
    优化login的错误提示。

--- app/settings.py
+++ app/settings.py
@@ -101,6 +101,39 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+MDEDITOR_CONFIGS = {
+    'default': {
+        'width': '90% ',  # Custom edit box width
+        'heigth': 500,  # Custom edit box height
+        'toolbar': ["undo", "redo",  # "|",
+                    # "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|",
+                    # "h1", "h2", "h3", "h5", "h6", "|",
+                    # "list-ul", "list-ol", "hr", "|",
+                    # "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table", "datetime"
+                    # "emoji", "html-entities", "pagebreak", "goto-line", "|",
+                    # "help", "info",
+                    # "||", "preview", "watch", "fullscreen"],
+                    ],
+        # custom edit box toolbar
+        # image upload format type
+        'upload_image_formats': ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
+        'image_folder': 'editor',  # image save the folder name
+        'theme': 'default',  # edit box theme, dark / default
+        'preview_theme': 'default',  # Preview area theme, dark / default
+        'editor_theme': 'default',  # edit area theme, pastel-on-dark / default
+        'toolbar_autofixed': True,  # Whether the toolbar capitals
+        'search_replace': True,  # Whether to open the search for replacement
+        'emoji': True,  # whether to open the expression function
+        'tex': True,  # whether to open the tex chart function
+        'flow_chart': True,  # whether to open the flow chart function
+        'sequence': False,  # Whether to open the sequence diagram function
+        'watch': False,  # Live preview
+        'lineWrapping': False,  # lineWrapping
+        'lineNumbers': True  # lineNumbers
+    }
+
+}
+
 # Use some debug tools in development.
 if DEBUG:
     INSTALLED_APPS.append('debug_toolbar')
